3b9be5b371fcf2d5dd35a8ce7ee7365da33dcf9f
BrailleBennett <103331457+BrailleBennett@users.noreply.github.com>
2024-09-03 02:00:16 -0500

    Updated YTDLP version to latest

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs::write;
 use reqwest::blocking::get;
 
-const YTDLP_VERSION: &str = "2024.08.01";
+const YTDLP_VERSION: &str = "2024.08.06";
 
 fn main() {
     if env::var("CARGO_CFG_UNIX").is_err() {
